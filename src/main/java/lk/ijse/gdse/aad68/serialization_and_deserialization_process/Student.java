1394a14c565b7d4b42e4b581fd83cfe33279e9d4
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2024-07-18 22:57:09 +0530

    Update Student  class

--- src/main/java/lk/ijse/gdse/aad68/serialization_and_deserialization_process/Student.java
+++ src/main/java/lk/ijse/gdse/aad68/serialization_and_deserialization_process/Student.java
@@ -20,7 +20,7 @@ public class Student implements Serializable {
 
         @Override
         public String toString() {
-            return "Person{name='" + name + "',address='" + address + "',age='" + age + '}';
+            return "Student{name='" + name + "',address='" + address + "',age='" + age + '}';
         }
     }
 
